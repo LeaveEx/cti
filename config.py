ed5bf304dfc0be865da96c9ada2c9476c0c66aa2
LeaveEx <126212388+LeaveEx@users.noreply.github.com>
2023-03-09 06:28:12 +0700

    Update config.py

--- config.py
+++ config.py
@@ -5,14 +5,14 @@ load_dotenv() if not path.exists("local.env") else load_dotenv("local.env")
 
 
 class Config:
-    api_id = int(getenv("API_ID", "14672956"))
-    api_hash = getenv("API_HASH", "115e8242ea0423893160bb61a9e05eab")
-    bot_token = getenv("BOT_TOKEN", "5937294943:AAF__32hQvheNIGI8D3MiTr30HNhXm56oF8")
-    log_channel = int(getenv("LOG_CHANNEL"))
-    fsub_chid = int(getenv("FORCESUB_CHANNEL"))
-    db_chid = int(getenv("DB_CHANNEL"))
+    api_id = int(getenv("API_ID", "2857053"))
+    api_hash = getenv("API_HASH", "4a01e3596661ba4bf609d15c1f9e129b")
+    bot_token = getenv("BOT_TOKEN", "6176925571:AAE580KWJZm1scSYh-ltJcd0xsBC-Ytm0Ns")
+    log_channel = int(getenv("LOG_CHANNEL", "-1001515293309"))
+    fsub_chid = int(getenv("FORCESUB_CHANNEL", "-1001515293309"))
+    db_chid = int(getenv("DB_CHANNEL", "-1001691630680"))
     blacklisted_channel = [int(x) for x in getenv("BLACKLISTED_CHANNEL", "-1001874961981").split(",") if x is not None]
-    channel1 = int(getenv("CHANNEL_1"))
+    channel1 = int(getenv("CHANNEL_1", "-1001515293309"))
     channel2 = int(getenv("CHANNEL_2"))
     channel3 = int(getenv("CHANNEL_3"))
 
