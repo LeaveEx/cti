8c743e30f866e7f2726b4876d88fd61e80e3f783
LeaveEx <126212388+LeaveEx@users.noreply.github.com>
2023-03-09 22:11:13 +0700

    Update bot.py

--- bot.py
+++ bot.py
@@ -85,7 +85,7 @@ def garz(message):
 # handling teks menfess cuy
 @bot.message_handler(content_types=["text"])
 def menfessin(message):
-	id = message.teks
+	id = message.chat
 	teks = message.text
 	ah = tegar(teks)
 	ih = len(teks.split(" "))
