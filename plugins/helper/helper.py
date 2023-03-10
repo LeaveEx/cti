b52fdf6a7c32ce3fd517c102f4ce7f773b83a178
LeaveEx <126212388+LeaveEx@users.noreply.github.com>
2023-03-10 07:14:38 +0700

    Update helper.py

--- plugins/helper/helper.py
+++ plugins/helper/helper.py
@@ -59,6 +59,7 @@ class Helper():
             [InlineKeyboardButton('Coba lagi', url=f'https://t.me/{self.bot.username}?start=start')]
         ])
         await self.bot.send_message(self.user_id, config.pesan_join, reply_to_message_id=self.message.id, reply_markup=markup)
+        
 
     async def daftar_pelanggan(self):
         database = Database(self.user_id)
