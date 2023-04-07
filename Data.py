844a84c196a7128508b87bc323d00d703958050a
LeaveEx <126212388+LeaveEx@users.noreply.github.com>
2023-04-07 10:39:58 +0700

    Update Data.py

--- Data.py
+++ Data.py
@@ -3,12 +3,12 @@ from pyrogram.types import InlineKeyboardButton
 class Data:
     HELP = """
 <b> ❏ Silahkan kirim pesan anda menggunakan hastag dibawah ini:
-#ctnboy (Untuk Identitas Laki-Laki)
-#ctngirl (Untuk Identitas perempuan)
-#ctnrandom (Untuk hal random)
-#ctnmutualan (Untuk mutualan)
-#ctnask (Untuk membagikan hal bodoh)
-#ctncurhat (Untuk curhat masalah)
+#ksboy (Untuk Identitas Laki-Laki)
+#ksgirl (Untuk Identitas perempuan)
+#ksrandom (Untuk hal random)
+#ksmutualan (Untuk mutualan)
+#ksask (Untuk membagikan hal bodoh)
+#kscurhat (Untuk curhat masalah)
 """
 
     close = [
@@ -36,9 +36,9 @@ RULES :
 3. DILARANG PROMOSI LINK GROUP / LPM / BOT LAIN
 4. DILARANG FAKE PROMOTE MENGGUNAKAN USERNAME ORANG LAIN
 5. DILARANG SHARE NO WHATSAPP
-6. DILARANG OPEN VCS / JUAL KONTEN SELAIN TALENT RESMI FWBESS
+6. DILARANG OPEN VCS / JUAL KONTEN SELAIN TALENT RESMI KAMISATO
 7, DILARANG JUAL DIRI
 — MELANGGAR = AUTO BANNED —
 INFO UNTUK MEMBER
-Semua pesan akan terkirim ke grup dan ke channel @caritemangobrol jika ada yang melanggar rules atau menyebarkan suati privasi orang bisa kalian laporkan ke admin atau ke owner @istmeleave
+Semua pesan akan terkirim ke grup dan ke channel @menfessks jika ada yang melanggar rules atau menyebarkan suatu privasi orang bisa kalian laporkan ke admin atau ke owner @istmeleave
 """
