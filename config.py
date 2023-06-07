60f116436e0140796d8d17cc690aa7152809ef21
LeaveEx <126212388+LeaveEx@users.noreply.github.com>
2023-06-08 01:29:02 +0700

    Update config.py

--- config.py
+++ config.py
@@ -14,7 +14,7 @@ channel_2 = int(os.environ.get("CHANNEL_2", "-1001215547362")) #untuk group come
 channel_log = int(os.environ.get("CHANNEL_LOG", "-1001515293309"))
 # =========================================================== #
 
-id_admin = int(os.environ.get("ID_ADMIN", "742495738"))
+id_admin = int(os.environ.get("ID_ADMIN", ""))
 # =========================================================== #
 
 batas_kirim = int(os.environ.get("BATAS_KIRIM", "2"))
